a28a88d5b5d3f4456c0d911dbbc576901366c90e
TomekGrzybek <t.grzybowski94@gmail.com>
2022-12-09 11:59:31 +0100

    Tasks 2-3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,13 +2,15 @@
 const mainList = document.querySelector("#categories");
 
 console.log(`Number of categories: ${mainList.children.length}`);
-console.log(mainList.children);
+console.log(""); //For a nice break in console
 
 for (let i = 0; i <= mainList.children.length - 1; i++) {
+  //iterate trough each li
   const category = mainList.children[i];
-  const title = category.children[0].textContent;
-  const categoryElems = category.children[1].children.length;
+  const title = category.children[0].textContent; //select header content
+  const categoryElems = category.children[1].children.length; //count items in category
 
   console.log(`Category: ${title}`);
   console.log(`Elements: ${categoryElems} `);
+  console.log(""); //For a nice break in console
 }
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const liArray = [];
+
+ingredients.forEach((el) => {
+  const li = document.createElement("li");
+  li.textContent = el;
+  li.classList.add("item");
+
+  liArray.push(li); //Would it not be better to append it one by one here instead of creating an array?
+});
+
+list.append(...liArray);
